7d9283ff63065c058dad780c280c9248829c77b3
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-07-14 21:55:13 +0530

    FlavourVerse server initiated

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to flavour-verse server!" });
+});
+
+app.listen(3000, () => {
+   console.log('Listening on port 3000');
+});
